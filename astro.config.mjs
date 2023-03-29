ffbd2ab357afa24ac8cb6c4f18fa43b2e4347af8
ilgimozturk <eakkaya21@ku.edu.tr>
2023-03-29 07:08:01 +0200

    ezgiakkaya.co

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://ezgiakkaya.github.io',
-  base: '/my-repo',
+  base: '/ezgiakkaya.github.io/',
 });
